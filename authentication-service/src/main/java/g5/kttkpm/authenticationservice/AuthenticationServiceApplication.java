4087ce4e18c6c463db37388e25e26713264aae69
Lê Minh Bảo <besilent308@gmail.com>
2025-02-25 09:47:44 +0700

    feat(authentication): Create authentication module

---
+++ authentication-service/src/main/java/g5/kttkpm/authenticationservice/AuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package g5.kttkpm.authenticationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationServiceApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationServiceApplication.class, args);
+    }
+    
+}
