8daedd7f0c740f8b3da08facfd65270213659e38
nguyenduc <ngduc0106@gmail.com>
2025-02-27 22:49:19 +0700

    feat(Category-Service): create package: controller,service,entity(class Category)

---
+++ category-service/src/main/java/entity/Category.java
@@ -0,0 +1,41 @@
+package entity;
+
+import jakarta.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(UUID id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    // Getters and Setters
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
