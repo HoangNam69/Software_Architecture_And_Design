5201654982e0a5ed9ab6c24930a4d7f8ff6138df
Lê Minh Bảo <besilent308@gmail.com>
2025-03-18 14:39:24 +0700

    feat(order-service): Create base order service

---
+++ order-service/src/test/java/g5/kttkpm/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package g5.kttkpm.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
