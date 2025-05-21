c4efe97c9c83c73976699ddbf042fbf97fa6ed59
Nam Le Hoang <lehoangnam.31123@gmail.com>
2025-05-21 11:31:45 +0700

    feat(report-service): handle export pdf - config font for jasper

--- report-service/src/main/java/iuh/fit/reportservice/dtos/request/ReportItemRequestDTO.java
+++ report-service/src/main/java/iuh/fit/reportservice/dtos/request/ReportItemRequestDTO.java
@@ -1,6 +1,35 @@
 package iuh.fit.reportservice.dtos.request;
 
-public record ReportItemRequestDTO(
-        String key, String productName, Integer quantity, Double revenue
-) {
+public class ReportItemRequestDTO {
+
+    private String productName;
+    private int quantity;
+    private double revenue;
+
+    public ReportItemRequestDTO() {
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getRevenue() {
+        return revenue;
+    }
+
+    public void setRevenue(double revenue) {
+        this.revenue = revenue;
+    }
 }
